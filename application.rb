c1c58bd7151f788a2a56bda3078b627780aba011
Dat Le-Tien <vnkeymaster@gmail.com>
2018-12-18 14:59:52 +0700

    Preload highlevel dependencies first in Application

--- application.rb
+++ application.rb
@@ -1,6 +1,8 @@
 require 'slack-ruby-bot'
 require 'app/bot'
 
+Dir.glob('./app/{helpers,controllers,commands,uncommands,services}/*.rb').each { |file| require file }
+
 Dir[File.join(__dir__, '/app/**/*.rb')].each do |file|
   require file
 end
