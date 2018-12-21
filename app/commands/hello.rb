281fb22e9d501d2098c0cbf1ac8043a55da4ce1c
Dat Le-Tien <vnkeymaster@gmail.com>
2018-12-21 16:44:27 +0700

    Update command hello

--- app/commands/hello.rb
+++ app/commands/hello.rb
@@ -3,7 +3,9 @@ module App
     # simple command
     class Hello < SlackRubyBot::Commands::Base
       command 'hello' do |client, data, _match|
-        client.say(channel: data.channel, text: 'bonjour :awesome:')
+        client.say(channel: data.channel,
+                    thread_ts: data.thread_ts,
+                    text: 'bonjour :awesome:')
       end
     end
   end
